d5b7c8c5cbfc96f36f1322d23610f857f368a7bb
tosh1ue <tosh1ue@qq.com>
2026-03-06 23:56:00 +0800

    init commit

--- example.c
+++ example.c
@@ -1,8 +1,16 @@
 #include "draw_scramble/draw_scramble.h"
 #include <stdio.h>
 
-void main()  {
-  printf("hello world\n");
+void main(void)  {
+
+  // F L R B
+  printf("+-----------+\n");
+  printf("| %d | %d | %d |\n", 3, 3, 3);
+  printf("+-----------+\n");
+  printf("| %d | %d | %d |\n", 3, 3, 3);
+  printf("+-----------+\n");
+  printf("| %d | %d | %d |\n", 3, 3, 3);
+  printf("+-----------+\n");
 
   while (1) {
 
